d760dbf6bf75973c114375e73188eb254b59a3f1
unknown <a00516283@gmail.com>
2019-03-01 00:27:42 -0600

    Servidor listo para funcionar

---
+++ classes/server.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+import http from 'http';
+
+export default class Server {
+    private static _instance: Server;
+    public app: express.Application;
+    private httpServer: http.Server;
+
+    private constructor(){
+        this.app = express();
+        this.httpServer = new http.Server(this.app);
+    }
+
+    public static get instance(){
+        return this._instance || (this._instance = new this());
+    }
+
+    start(callback: Function, port: number){
+        this.httpServer.listen(port, callback);
+    }
+}
--- index.ts
+++ index.ts
@@ -1,1 +1,23 @@
-console.log("Hola jose alfredo");
+import Server from './classes/server';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+const server  = Server.instance;
+
+//Body parser
+server.app.use(bodyParser.urlencoded({extended: true}));
+server.app.use(bodyParser.json());
+
+//Configuracion del cors
+server.app.use(cors({
+    origin: true,
+    credentials: true
+}));
+
+server.app.get("/", (req, res) => {
+    res.send("Hola mundo");
+});
+
+server.start(() => {
+    console.log("Servidor corriendo en el puerto 5000");
+}, 5000);
